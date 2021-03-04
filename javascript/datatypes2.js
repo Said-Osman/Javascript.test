b6bdac48ceb3da22b601195b7dd2c59bc5a0a294
said osman <said_henry@hotmail..com>
2021-03-04 18:23:21 +0100

    new update

--- javascript/datatypes2.js
+++ javascript/datatypes2.js
@@ -1,4 +1,14 @@
-let name = ["tom", "lars", "nick"]
-console.log(name[2]) 
+const x = 10;
 
-dsd 
+if (x <= 10) {
+  console.log('YES 10 true Welcome to club Mr. Coffee');
+ } else if (x == 14) {
+  console.log('YES 14 true Welcome to club Mr. Coffee');
+ }  else if (x >= 11) {
+  console.log('YES 11 true Welcome to club Mr. Coffee');
+}     else if (x === 12) {
+  console.log('YES 12 true and Welcome to club Mr. Coffee');
+}  else  {
+ console.log('Sorry but you are NOT Mr. Coffee, Adios amigos haha');
+
+}
